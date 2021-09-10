566d73f01a2688fe233a4738d881bb3fe2f0b1ec
Sergey <rumyantsev.serge@yandex.ru>
2021-09-10 10:39:19 +0300

    Initial Commit

---
+++ ColorMixSwiftUIApp/ColorMixSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixSwiftUIAppApp.swift
+//  ColorMixSwiftUIApp
+//
+//  Created by Sergey on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
